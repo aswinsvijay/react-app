9a9d6904b24b917e856fd505d6168da69a476dfc
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-04-06 16:04:28 -0500

    move utils to folder

--- src/Clock/App.tsx
+++ src/Clock/App.tsx
@@ -1,50 +1,12 @@
 import './App.css';
 import React, { useMemo, useState } from 'react';
 import { variants } from './variants';
+import { getTimeZoneOffsetString } from './utils';
 
 type TimeZoneConfig = {
   label: string;
   timeZoneId: string;
 };
-
-function divMod(value: number, divisor: number) {
-  return [Math.floor(value / divisor), Math.floor(value % divisor)] as const;
-}
-
-function range(start: number, stop: number) {
-  const res = [];
-
-  for (let i = start; i <= stop; ++i) {
-    res.push(i);
-  }
-
-  return res;
-}
-
-function splitTensAndOnes(value: number) {
-  return divMod(value, 10);
-}
-
-function getTimeZoneOffset(timeZoneId: string) {
-  const date = new Date();
-
-  const utcDate = new Date(date.toLocaleString('en-US', { timeZone: 'UTC' }));
-  const tzDate = new Date(date.toLocaleString('en-US', { timeZone: timeZoneId }));
-
-  return (tzDate.getTime() - utcDate.getTime()) / (1000 * 60);
-}
-
-function getTimeZoneOffsetString(timeZoneId: string) {
-  const timeZoneOffsetMinutes = getTimeZoneOffset(timeZoneId);
-
-  const sign = timeZoneOffsetMinutes < 0 ? '-' : '+';
-  const absoluteOffset = Math.abs(timeZoneOffsetMinutes);
-
-  const [hours, minutes] = divMod(absoluteOffset, 60);
-  const [hoursString, minutesString] = [`${hours}`, `${minutes}`];
-
-  return `UTC${sign}${hoursString.padStart(2, '0')}:${minutesString.padStart(2, '0')}`;
-}
 
 const configs: TimeZoneConfig[] = [
   {
@@ -87,7 +49,7 @@ const App: React.FC<NonNullable<unknown>> = () => {
               <div
                 className="background"
                 style={{
-                  backgroundImage: `url(public/clock/${config.timeZoneId.toLowerCase().replace('/', '-')}.jpg)`,
+                  backgroundImage: `url(/clock/${config.timeZoneId.toLowerCase().replace('/', '-')}.jpg)`,
                 }}
               ></div>
               <div className="clock-with-label">
---
+++ src/Clock/utils.ts
@@ -0,0 +1,38 @@
+export function divMod(value: number, divisor: number) {
+  return [Math.floor(value / divisor), Math.floor(value % divisor)] as const;
+}
+
+export function range(start: number, stop: number) {
+  const res = [];
+
+  for (let i = start; i <= stop; ++i) {
+    res.push(i);
+  }
+
+  return res;
+}
+
+export function splitTensAndOnes(value: number) {
+  return divMod(value, 10);
+}
+
+export function getTimeZoneOffset(timeZoneId: string) {
+  const date = new Date();
+
+  const utcDate = new Date(date.toLocaleString('en-US', { timeZone: 'UTC' }));
+  const tzDate = new Date(date.toLocaleString('en-US', { timeZone: timeZoneId }));
+
+  return (tzDate.getTime() - utcDate.getTime()) / (1000 * 60);
+}
+
+export function getTimeZoneOffsetString(timeZoneId: string) {
+  const timeZoneOffsetMinutes = getTimeZoneOffset(timeZoneId);
+
+  const sign = timeZoneOffsetMinutes < 0 ? '-' : '+';
+  const absoluteOffset = Math.abs(timeZoneOffsetMinutes);
+
+  const [hours, minutes] = divMod(absoluteOffset, 60);
+  const [hoursString, minutesString] = [`${hours}`, `${minutes}`];
+
+  return `UTC${sign}${hoursString.padStart(2, '0')}:${minutesString.padStart(2, '0')}`;
+}
