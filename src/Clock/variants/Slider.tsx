daec74401108cc90b0cb23233e9cd811cb751438
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-04-06 23:14:03 -0500

    finish ScrollingClock implementation

--- src/Clock/variants/Slider.tsx
+++ src/Clock/variants/Slider.tsx
@@ -1,6 +1,33 @@
 import type React from 'react';
 import type { ClockComponentProps } from '../types';
+import { range } from '../utils';
+
+const limits = [range(0, 2), range(0, 9), range(0, 5), range(0, 9), range(0, 5), range(0, 9)];
 
 export const Slider: React.FC<ClockComponentProps> = ({ timeParts }) => {
-  return <div className="scrolling-clock-container">{timeParts}</div>;
+  return (
+    <div className="scrolling-clock-container">
+      {([0, 1, 2, 3, 4, 5] as const).map((i) => {
+        const rng = limits[i];
+        const current = timeParts[i];
+
+        return (
+          <div
+            className="rng-container"
+            style={{
+              marginTop: `calc(-1 * ${current} * var(--cell-size))`,
+            }}
+          >
+            {rng.map((i) => {
+              return (
+                <div className={`number-container${i === current ? ' active' : ''}`}>
+                  <div className={'inner'}>{i}</div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
