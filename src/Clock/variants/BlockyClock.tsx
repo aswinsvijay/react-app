1f124a3b05305356c365d16d01411a1267157613
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-04-06 23:54:36 -0500

    implement BlockyClock

--- src/Clock/variants/BlockyClock.tsx
+++ src/Clock/variants/BlockyClock.tsx
@@ -1,6 +1,111 @@
 import type React from 'react';
 import type { ClockComponentProps } from '../types';
 
+const digitMap: Record<'_' | number, [number, number, number][]> = {
+  _: [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ],
+  0: [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
+  1: [
+    [2, 1, 0],
+    [2, 1, 0],
+    [2, 1, 0],
+    [2, 1, 0],
+    [2, 1, 0],
+  ],
+  2: [
+    [0, 0, 0],
+    [2, 1, 0],
+    [0, 0, 0],
+    [0, -1, -2],
+    [0, 0, 0],
+  ],
+  3: [
+    [0, 0, 0],
+    [2, 1, 0],
+    [0, 0, 0],
+    [2, 1, 0],
+    [0, 0, 0],
+  ],
+  4: [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+    [2, 1, 0],
+    [2, 1, 0],
+  ],
+  5: [
+    [0, 0, 0],
+    [0, -1, -2],
+    [0, 0, 0],
+    [2, 1, 0],
+    [0, 0, 0],
+  ],
+  6: [
+    [0, 0, 0],
+    [0, -1, -2],
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
+  7: [
+    [0, 0, 0],
+    [2, 1, 0],
+    [2, 1, 0],
+    [2, 1, 0],
+    [2, 1, 0],
+  ],
+  8: [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
+  9: [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+    [2, 1, 0],
+    [0, 0, 0],
+  ],
+};
+
+const BlockyDigit: React.FC<{ digit: number }> = ({ digit }) => {
+  return (
+    <div className="pixel-grid">
+      {digitMap[digit].flat().map((value) => {
+        return (
+          <div
+            className="pixel-wrapper"
+            style={{
+              transform: `translate(calc(${value} * 100%))`,
+            }}
+          >
+            <div className="pixel square"></div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export const BlockyClock: React.FC<ClockComponentProps> = ({ timeParts }) => {
-  return <div className="blocky-clock-container">{timeParts}</div>;
+  return (
+    <div className="blocky-clock-container">
+      {timeParts.map((digit) => (
+        <BlockyDigit digit={digit} />
+      ))}
+    </div>
+  );
 };
