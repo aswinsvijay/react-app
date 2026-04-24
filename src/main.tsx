03d3a3141f8cdc8bafc12983ff79e2dcdca607e5
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2026-04-24 01:10:57 -0500

    add notistack provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { SnackbarProvider } from 'notistack';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SnackbarProvider anchorOrigin={{ horizontal: 'right', vertical: 'top' }}>
+      <App />
+    </SnackbarProvider>
   </StrictMode>
 );
